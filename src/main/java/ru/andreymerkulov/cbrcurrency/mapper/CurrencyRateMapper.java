769b409986ad0e39d8b934cec2a40427f9a3f0c2
Andrey Merkulov <andrew_aa@mail.ru>
2024-08-20 20:32:25 +0300

    испрвления по замечаниям

--- src/main/java/ru/andreymerkulov/cbrcurrency/mapper/CurrencyRateMapper.java
+++ src/main/java/ru/andreymerkulov/cbrcurrency/mapper/CurrencyRateMapper.java
@@ -2,12 +2,14 @@ package ru.andreymerkulov.cbrcurrency.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import ru.andreymerkulov.cbrcurrency.dto.CbrResponse;
 import ru.andreymerkulov.cbrcurrency.dto.CbrValute;
 import ru.andreymerkulov.cbrcurrency.model.Currency;
 import ru.andreymerkulov.cbrcurrency.model.CurrencyRate;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring"
+        , unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface CurrencyRateMapper {
 
     @Mapping(target = "id.numCode", source = "currency.numCode")
